42e21cf23eb2f0fb595102f7ce2e041ad0b8a4b6
David Casanellas <davidcasanell@gmail.com>
2017-05-26 18:33:52 +0200

    feat: CSS Movie component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  textButton = ['Pick this..', 'Or pick this!'];
+  movies = [
+    {
+      "poster_path": "/tWqifoYuwLETmmasnGHO7xBjEtt.jpg",
+      "adult": false,
+      "overview": "A live-action adaptation of Disney's version of the classic 'Beauty and the Beast' tale of a cursed prince and a beautiful young woman who helps him break the spell.",
+      "release_date": "2017-03-16",
+      "genre_ids": [
+        10402,
+        10751,
+        14,
+        10749
+      ],
+      "id": 321612,
+      "original_title": "Beauty and the Beast",
+      "original_language": "en",
+      "title": "Beauty and the Beast",
+      "backdrop_path": "/7QshG75xKCmClghQDU1ta2BTaja.jpg",
+      "popularity": 150.703083,
+      "vote_count": 2709,
+      "video": false,
+      "vote_average": 6.8
+    },
+    {
+      "poster_path": "/y4MBh0EjBlMuOzv9axM4qJlmhzz.jpg",
+      "adult": false,
+      "overview": "The Guardians must fight to keep their newfound family together as they unravel the mysteries of Peter Quill's true parentage.",
+      "release_date": "2017-04-24",
+      "genre_ids": [
+        12,
+        28,
+        35,
+        878
+      ],
+      "id": 283995,
+      "original_title": "Guardians of the Galaxy Vol. 2",
+      "original_language": "en",
+      "title": "Guardians of the Galaxy Vol. 2",
+      "backdrop_path": "/aJn9XeesqsrSLKcHfHP4u5985hn.jpg",
+      "popularity": 93.129598,
+      "vote_count": 1836,
+      "video": false,
+      "vote_average": 7.6
+    }
+  ];
 }
--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-movie',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie.component.css']
 })
 export class MovieComponent implements OnInit {
+  @Input()
+  movie;
+
+  @Input()
+  textButton;
 
   constructor() { }
 
